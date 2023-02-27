81324524196d09aa2f50dfb35f3b367dcedc7800
Leo9723 <pala9761@gmail.com>
2023-02-27 15:49:51 +0100

    aggiunta db

---
+++ database/seeders/MovieTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Movie;
+
+class MovieTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $movies = config('Movies');
+
+        foreach($movies as $movie){
+
+            $newmovie = new Movie();
+
+            $newmovie->title = $movie['title'];
+            $newmovie->original_title = $movie['original_title'];
+            $newmovie->nationality = $movie['nationality'];
+            $newmovie->release_date = $movie['release_date'];
+            $newmovie->vote = $movie['vote'];
+            $newmovie->cast = $movie['cast'];
+            $newmovie->cover_path = $movie['cover_path'];
+
+            $newmovie->save();
+
+
+        }
+
+    }
+}
